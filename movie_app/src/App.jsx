958f95654c537954ae9fcb281603dddc5bc6d71b
kinzydev <kegladesign@gmail.com>
2026-05-04 21:32:32 +0200

    will not do database for app, only the fron-end. database will come in future updates

--- movie_app/src/App.jsx
+++ movie_app/src/App.jsx
@@ -1,28 +1,53 @@
 import { useState, useEffect } from 'react';
 import Search from './components/Search';
+import Spinner from './components/Spinner';
+import MovieCard from './components/MovieCard';
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+const API_BASE_URL ='https://api.themoviedb.org/3';
+const API_KEY= import.meta.env.VITE_TMDB_API_KEY;
 const API_OPTIONS = {
   method:'GET',
   headers: {
-    accept: 'application/json'
+    accept: 'application/json',
+    Authorization:`Bearer ${API_KEY}`
   }
 
 }
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState('');
-  const [setLoading, setIsLoading] = useState('');
+  const [errorMessage, setErrorMessage] = useState('')
+  const [isLoading, setIsLoading] = useState(false)
+  const [movieList, setMovieList] = useState([]);
 
-  const fetchAnime = async () => {
+  const fetchMovies = async () => {
+    setIsLoading(true);
+    setErrorMessage('');
     try {
+      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`
+      const response = await fetch(endpoint, API_OPTIONS);
 
-    } catch {
+      if(!response.ok) {
+        throw new Error('Failed to fetch movies')
+      }
+      const data = await response.json();
+      if (data.Response === 'False') {
+        setErrorMessage(data.Error || 'Failed to fetch movies');
+        setMovieList([]);
+        return;
+      }
 
+      setMovieList(data.results || []);
+;
+    } catch(error) {
+      setErrorMessage('Error fetching movies. Please try again later.')
+    } finally {
+      setIsLoading(false);
     }
   }
   useEffect(() => {
-
+    fetchMovies();
   }, []);
 
  
@@ -33,10 +58,30 @@ const App = () => {
       <div className='pattern'>
         <div className='wrapper'>
           <header>
-            <img className='max-h-sm max-w-70  rounded-xl' src="./anime-hero.jpg" alt="Hero Banner" />
+            <img className='' src="./hero.png" alt="Hero Banner" />
             <h1>Find <span className='text-gradient'>Anime</span> You'll Enjoy Without The Hassle </h1>
+            <Search searchTerm ={searchTerm} setSearchTerm= {setSearchTerm} />
           </header>
-          <Search searchTerm ={searchTerm} setSearchTerm= {searchTerm} />
+          <section className='all-movies'>
+            <h2 className='mt-5'>All Movies</h2>
+
+
+            {isLoading ? (
+              <Spinner />
+              
+            ) : errorMessage ? (
+              <p className='text-red-500'>{errorMessage}</p>
+            ) : (
+              <ul>
+                {movieList.map((movie) => (
+                <MovieCard key={movie.id} movie={movie}/>
+                ))}
+              </ul>
+            )
+            }
+
+          </section>
+          
         </div>
       </div>
 
